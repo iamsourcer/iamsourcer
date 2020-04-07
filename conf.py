9280b97e7b7fe263374147f805038c6b180b4017
Jonathan Tarrico <tarricoj@gmail.com>
2020-04-06 22:01:53 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -153,11 +153,11 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "bootblog"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
-THEME_COLOR = '#5670d4'
+THEME_COLOR = '#033C73'
 
 # Theme configuration. Fully theme-dependent. (translatable)
 # Examples below are for bootblog4.
